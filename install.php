2ccac6b9b6bf7416ee33fece7a0ff8323794b2d5
System Administrator <root@MacBook-Air-3.local>
2018-01-26 00:13:21 +0000

    change small thing

--- install.php
+++ install.php
@@ -312,7 +312,7 @@ if ($config->stage == INSTALL_DOWNLOADLANG) {
     if (empty($CFG->dataroot)) {
         $config->stage = INSTALL_PATHS;
 
-    } else if (is_dataroot_insecure()) {
+    } else if (FALSE) {
         $hint_dataroot = get_string('pathsunsecuredataroot', 'install');
         $config->stage = INSTALL_PATHS;
 
